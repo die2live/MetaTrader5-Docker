574d6125bb63e4098b4569fbe3345014e48b50d5
Alexandr Dermenji <alexandr.dermenji@amdaris.com>
2026-02-26 18:00:09 +0200

    back to 10.3.0/wine-mono-10.3.0-x86.msi

--- Metatrader/start.sh
+++ Metatrader/start.sh
@@ -8,7 +8,7 @@ wine_executable="wine"
 metatrader_version="5.0.36"
 mt5server_port="8001"
 MT5_CMD_OPTIONS="${MT5_CMD_OPTIONS:-}"
-mono_url="https://dl.winehq.org/wine/wine-mono/11.0.0/wine-mono-11.0.0-x86.msi"
+mono_url="https://dl.winehq.org/wine/wine-mono/10.3.0/wine-mono-10.3.0-x86.msi"
 python_url="https://www.python.org/ftp/python/3.9.13/python-3.9.13.exe"
 mt5setup_url="https://download.mql5.com/cdn/web/metaquotes.software.corp/mt5/mt5setup.exe"
 
